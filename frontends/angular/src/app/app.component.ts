02c263e93eb5ad98926fd55de5961dc72e82b44e
Saurabh Srivastava <vasudeveloper001@gmail.com>
2022-01-27 21:11:30 +0530

    Frontend for Angular Traditional Manner Completed

---
+++ frontends/angular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  public appName: string = 'Keyboard NFT Minter';
+  public constructor() {}
+
+  public ngOnInit() {}
+
+  public ngOnDestroy() {}
+}
